edf0cbac04d4821cab727a917b5d5cd343d7fe12
yingjianjian <yingjj@isyscore.com>
2019-11-12 08:34:36 +0800

    add README

--- log_mail/util/kibana_api.py
+++ log_mail/util/kibana_api.py
@@ -1,8 +1,13 @@
 import json
 from urllib.request import Request, urlopen
 from kibana_sentinl_mail.settings import KIBANA_URL
+import sys
 url = "%s/api/saved_objects/_find?type=index-pattern&per_page=10000"  %(KIBANA_URL)
 firefox_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 request = Request( url, headers=firefox_headers )
 html = urlopen(request)
-data = json.loads(str(html.read(),'utf-8'))['saved_objects'][0]['id']
+try:
+    data = json.loads(str(html.read(),'utf-8'))['saved_objects'][0]['id']
+except IndexError as e:
+    print("kibana没有找到object对象请先创建索引")
+    sys.exit(1)
