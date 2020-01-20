461fe1d850eb4215758f523d993861b430c308b8
yingjianjian <yingjj@isyscore.com>
2020-01-20 14:04:18 +0800

    rename project

--- ops_notfiy/settings.py
+++ ops_notfiy/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'ops_notfiy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
